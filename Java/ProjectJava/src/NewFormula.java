9c0c8253138bcb0e7b99d0ff9b2493e003d9c470
Dmitriy <135806057+ExampleName404@users.noreply.github.com>
2025-01-24 17:52:02 +0500

    Create NewFormula.java

---
+++ Java/ProjectJava/src/NewFormula.java
@@ -0,0 +1,9 @@
+public class NewFormula {
+    public static void main(String[] args) {
+        // Вычисление формулы
+        double result = 4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11));
+
+        // Вывод результата
+        System.out.println("Результат: " + result);
+    }
+}
